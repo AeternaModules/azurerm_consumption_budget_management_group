05a0443bf788745567f9b5abde19d14f74bd4b47
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:36:40 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_consumption_budget_management_group" "consumption_budget_management_groups" {
+  for_each = var.consumption_budget_management_groups
+
+  amount              = each.value.amount
+  management_group_id = each.value.management_group_id
+  name                = each.value.name
+  etag                = each.value.etag
+  time_grain          = each.value.time_grain
+
+  notification {
+    contact_emails = each.value.notification.contact_emails
+    enabled        = each.value.notification.enabled
+    operator       = each.value.notification.operator
+    threshold      = each.value.notification.threshold
+    threshold_type = each.value.notification.threshold_type
+  }
+
+  time_period {
+    end_date   = each.value.time_period.end_date
+    start_date = each.value.time_period.start_date
+  }
+
+  dynamic "filter" {
+    for_each = each.value.filter != null ? [each.value.filter] : []
+    content {
+      dynamic "dimension" {
+        for_each = filter.value.dimension != null ? [filter.value.dimension] : []
+        content {
+          name     = dimension.value.name
+          operator = dimension.value.operator
+          values   = dimension.value.values
+        }
+      }
+      dynamic "tag" {
+        for_each = filter.value.tag != null ? [filter.value.tag] : []
+        content {
+          name     = tag.value.name
+          operator = tag.value.operator
+          values   = tag.value.values
+        }
+      }
+    }
+  }
+}
+
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "consumption_budget_management_groups" {
+  description = "All consumption_budget_management_group resources"
+  value       = azurerm_consumption_budget_management_group.consumption_budget_management_groups
+}
+
